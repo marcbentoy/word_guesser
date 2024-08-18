3e7b3068d9b8cc92ce64b73d38ab52f06f5c567b
vince <marc.bentoy@gmail.com>
2024-08-18 12:03:39 +0800

    feat: add automated generation

--- cmd/guesser/main.go
+++ cmd/guesser/main.go
@@ -13,6 +13,8 @@ func main() {
 	populationCount := 1000
 	mutationRate := 0.01
 
+	isAutomated := false
+
 	guesser := wg.NewGuesser(targetPhrase, populationCount, float32(mutationRate))
 
 	fmt.Println("Initializing Population.")
@@ -22,6 +24,19 @@ func main() {
 
 	for {
 		fmt.Println()
+		if isAutomated {
+			guesser.Iterate()
+			fmt.Println("Generation: ", guesser.GenerationCount)
+			fmt.Println("\nBest: ")
+			wg.PrintMonkey(guesser.BestMonkey)
+
+			if guesser.BestMonkey.Score != len(targetPhrase) {
+				continue
+			}
+
+			fmt.Println("Best Monkey found: ", guesser.BestMonkey.Gene)
+			break
+		}
 
 		fmt.Print("> ")
 		command := ""
@@ -32,6 +47,17 @@ func main() {
 			fmt.Println("Initializing Population.")
 			guesser.Init()
 			fmt.Println("Population initialized.")
+			continue
+		}
+		if command == "a" {
+			if isAutomated {
+				fmt.Println("Auto generation stopped.")
+				isAutomated = false
+				continue
+			}
+			isAutomated = true
+
+			fmt.Println("Generating until maximum score is found..")
 			continue
 		}
 		if command == "n" || command == "" {
